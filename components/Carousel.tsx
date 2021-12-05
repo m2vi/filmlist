1ec1a1f9db05a4a60a271437ba32dcd82717ece6
m2vi <iuvo@gmx.at>
2021-12-05 14:12:52 +0100

    hm

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,4 +1,5 @@
 import { BrowseSectionProps } from '@utils/types';
+import Link from 'next/link';
 import { useTranslation } from 'react-i18next';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import Card from './Card';
@@ -7,14 +8,20 @@ const Carousel = ({ section: { items, length, name, route } }: { section: Browse
   const { t } = useTranslation();
 
   return (
-    <div className='mb-6' style={{ width: 'calc(100vw - 120px)', maxWidth: 'calc(2220px - 120px)' }}>
-      <h2 className='mb-2' style={{ fontSize: '25px!important' }}>
-        {t(`pages.filmlist.menu.${name.toLowerCase()}`)}
-      </h2>
+    <div className='mb-6 h-auto' style={{ width: 'calc(100vw - 120px)', maxWidth: 'calc(2220px - 120px)' }}>
+      {route ? (
+        <Link href={route} passHref={true}>
+          <a className='carousel-title hover:text-primary-200'>{t(`pages.filmlist.menu.${name.toLowerCase()}`)}</a>
+        </Link>
+      ) : (
+        <span className='carousel-title'>{t(`pages.filmlist.menu.${name.toLowerCase()}`)}</span>
+      )}
+
       <Swiper
         spaceBetween={20}
         slidesPerView={2}
         lazy={true}
+        className='mt-1'
         breakpoints={{
           2150: {
             slidesPerView: 13,
