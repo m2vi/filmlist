04684a85d0ad2138516e4a8c7ca0c5062c4e6945
m2vi <iuvo@gmx.at>
2021-12-06 22:15:00 +0100

    critical

--- utils/config.ts
+++ utils/config.ts
@@ -4,6 +4,6 @@ export const oauth = {
   cookieName: 'token',
   clientId: validateEnv('DISCORD_CLIENT_ID'),
   clientSecret: validateEnv('DISCORD_CLIENT_SECRET'),
-  jwtSecret: validateEnv('JWT_SECRET', 'bird is the word', true),
+  jwtSecret: validateEnv('JWT_SECRET'),
   allowedIDs: `${validateEnv('ALLOWED_IDS', '', true)}`.split(','),
 } as const;
