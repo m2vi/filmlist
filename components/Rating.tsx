0e209b371dae266e0d8b7c9f39a7da06ed926810
m2vi <iuvo@gmx.at>
2021-12-29 01:17:34 +0100

    g

--- components/Rating.tsx
+++ components/Rating.tsx
@@ -6,15 +6,23 @@ interface RatingProps extends Partial<FrontendItemProps> {
 }
 
 const Rating = ({ vote_average, notchild }: RatingProps) => {
+  const nr = !vote_average;
+
   return (
     <div
       className={`${
         !notchild ? 'absolute top-1 left-1 z-20' : ''
-      } p-1 w-auto flex items-center justify-center bg-black bg-opacity-80 rounded-5`}
+      } p-1 w-auto flex items-center justify-center bg-black bg-opacity-80 rounded-5 ${nr ? 'opacity-60' : ''}`}
     >
-      <span className='flex items-center text-amber-500 l-1'>
-        <StarIcon className='h-3 w-3 mr-1' />
-        {vote_average?.toFixed(1)}
+      <span className={`flex items-center l-1 ${nr ? 'text-primary-300' : 'text-amber-500'}`}>
+        {nr ? (
+          'NR'
+        ) : (
+          <>
+            <StarIcon className='h-3 w-3 mr-1' />
+            {vote_average?.toFixed(1)}
+          </>
+        )}
       </span>
     </div>
   );
