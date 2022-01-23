981da356184c53ca6bec3b557e3ab697f92c8631
m2vi <iuvo@gmx.at>
2022-01-23 12:33:40 +0100

    colors

--- pages/api/debug.ts
+++ pages/api/debug.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.json({});
+  res.json({
+    build_id: process.env.BUILD_ID,
+  });
 }
--- pages/api/temp/state.ts
+++ pages/api/temp/state.ts
@@ -5,5 +5,5 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const data = await api.schema.find().select('state').lean();
   const arr = removeDuplicates(data.map(({ state }) => state));
-  res.json(arr);
+  res.json(arr.sort((a, b) => a - b));
 }
