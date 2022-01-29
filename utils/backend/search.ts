03566266a9781d00a40cade6830de7b7a6da1817
m2vi <iuvo@gmx.at>
2022-01-29 16:39:42 +0100

    type fix

--- utils/backend/search.ts
+++ utils/backend/search.ts
@@ -69,6 +69,7 @@ class Search {
           external_ids: null,
           isMovie: item.media_type === 'movie',
           watchProviders: null,
+          omdb_base: null,
         });
       })
     );
