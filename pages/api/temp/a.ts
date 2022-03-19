e839352ef3737f50882c21e9e61ab6f8adb30e73
m2vi <iuvo@gmx.at>
2022-03-20 00:09:09 +0100

    fix

--- pages/api/temp/a.ts
+++
@@ -1,4 +0,0 @@
-import api from '@utils/similar/main';
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {}
