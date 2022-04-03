6457a9790af3ea56c543303fb62ee1eaf383a5d5
m2vi <iuvo@gmx.at>
2022-04-03 19:13:17 +0200

    fix

--- pages/api/person/[id].ts
+++ pages/api/person/[id].ts
@@ -1,11 +1,14 @@
 import filmlist from '@utils/apis/filmlist';
+import user from '@utils/user';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { id, locale = 'en' } = Object.freeze(req.query);
 
-    const data = await filmlist.getPerson(parseInt(id.toString()), locale.toString());
+    const client = await user.find(user.getIdFromRequest(req));
+
+    const data = await filmlist.getPerson(parseInt(id.toString()), locale.toString(), client);
 
     res.status(200).json(data);
   } catch (error) {
